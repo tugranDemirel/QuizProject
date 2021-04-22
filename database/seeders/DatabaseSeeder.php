f6939494a44f9b013feefada3f17866cb8185944
tugranDemirel <demireltugran66@gmail.com>
2021-04-22 13:32:58 +0300

    middleware hazırlanması, prefix ile route düzenlemesi, admin paneli türkçe route verme

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(5)->create();
+
+        User::insert([
+            'name'=>'Tuğran Demirel',
+            'email'=>'demireltugran66@gmail.com',
+            'type'=>'admin',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+Route::middleware(['auth', 'verified'])->get('/panel', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::group(['middleware'=>['auth', 'isAdmin'], 'prefix'=>'admin'], function (){
+    Route::get('deneme', function (){
+        return 'Prefix testidir. Prefix admin/deneme ye arar';
+    });
+});
