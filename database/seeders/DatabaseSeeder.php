891f566fe3e4db7016d191c720b0ea5e9f92b70f
tugranDemirel <demireltugran66@gmail.com>
2021-04-23 02:00:26 +0300

    Quiz tablosunu oluşturduk. Factory ve seed bağlantıları yapıldı. Quiz tablosuna ait model oluşturuldu

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
-use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,15 +15,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        \App\Models\User::factory(5)->create();
-
-        User::insert([
-            'name'=>'Tuğran Demirel',
-            'email'=>'demireltugran66@gmail.com',
-            'type'=>'admin',
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+        $this->call([
+            UserSeeder::class,
+            QuizSeeder::class,
         ]);
     }
 }
